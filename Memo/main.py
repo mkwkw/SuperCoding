5aa4efe9eee8c52144dfd128ecbcee5fa95acefe
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-03 12:00:33 +0900

    메모장 Update, Delete API 구현

--- Memo/main.py
+++ Memo/main.py
@@ -18,7 +18,24 @@ def create_memo(memo:Memo):
 @app.get('/memos')
 def read_memo():
     return memos
- 
-#Wordale 폴더 안에 있는 파일을 mount
+
+#req_memo id인 메모의 내용 수정
+@app.put('/memos/{memo_id}')
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id==req_memo.id:
+            memo.content = req_memo.content
+            return '성공했습니다.'
+    return '그런 메모는 없습니다.'
+
+#memo_id인 메모 삭제
+@app.delete('/memos/{memo_id}')
+def delete_memo(memo_id):
+    for index, memo in enumerate(memos): #enumerate: 배열의 인덱스와 값을 같이 뽑아주는 함수
+        if memo.id==memo_id:
+            memos.pop(index)
+            return '성공했습니다.'
+    return '그런 메모는 없습니다.'
+
 app.mount("/", StaticFiles(directory="SuperCoding/Memo/static", html=True), name="static")
 
--- Memo/static/app.js
+++ Memo/static/app.js
@@ -1,42 +1,84 @@
-function displayMemo(memo){
-    const ul = document.querySelector("#memo-ul");
-    const li = document.createElement("li");
-    //li.innerText = `[id:${memo.id}] ${memo.content}`;
-    li.innerText = `[채팅] ${memo.content}`;
-    ul.appendChild(li);
+async function deleteMemo(event) {
+  const id = event.target.dataset.id; //data-id
+  //내용 삭제 - DELETE
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
 }
 
-async function readMemo(){
-    const res = await fetch('/memos');
-    const jsonRes = await res.json(); //배열
-    const ul = document.querySelector("#memo-ul");
-    ul.innerHTML = "";
-    jsonRes.forEach(displayMemo); //배열의 원소 하나씩 참조
+async function editMemo(event) {
+  const id = event.target.dataset.id; //data-id
+  const editInput = prompt("수정할 값을 입력하세요!"); //alert창 같이 상단에 표시됨.
+  //내용 수정 - PUT
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      //문자열로 보냄
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
 }
 
-async function createMemo(value){
-    const res = await fetch("/memos",{
-        method:"POST",
-        headers:{
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ //문자열로 보냄
-            id: new Date().getTime(),
-            content: value,
-        }),
-    });
-    //const jsonRes = await res.json();
-    readMemo();
+function displayMemo(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  //memo.id를 전달하는 역할
+  editBtn.dataset.id = memo.id; //<div data-id={memo.id}> 이런 식으로 들어감
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제하기";
+  delBtn.addEventListener("click", deleteMemo);
+  //memo.id를 전달하는 역할
+  delBtn.dataset.id = memo.id; //<div data-id={memo.id}> 이런 식으로 들어감
+
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+  ul.appendChild(li);
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    const input = document.querySelector("#memo-input");
-    createMemo(input.value);
-    input.value = "";
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json(); //배열
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemo); //배열의 원소 하나씩 참조
 }
 
-const form = document.querySelector('#memo-form');
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      //문자열로 보냄
+      id: new Date().getTime(),
+      content: value,
+    }),
+  });
+  //const jsonRes = await res.json();
+  readMemo();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
 form.addEventListener("submit", handleSubmit); //form에서 제출 버튼이 눌렸을 때 동작
 
 readMemo();
