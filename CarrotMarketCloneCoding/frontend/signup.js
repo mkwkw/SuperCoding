e98b1d816c95db8e2d608dd1f69442f146eea6ab
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-04 21:33:58 +0900

    회원가입 기능 구현 - 해시

---
+++ CarrotMarketCloneCoding/frontend/signup.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("#signup-form");
+
+//비밀번호 확인
+const checkPassword = () => {
+  const formData = new FormData(form);
+  const password1 = formData.get("password");
+  const password2 = formData.get("password2");
+
+  if (password1 === password2) return true;
+  else return false;
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form); //id, pw 가져와서
+  const sha256Password = sha256(formData.get("password")); //pw 해시
+  formData.set("password", sha256Password); //해시한 pw 다시 넣기
+  //console.log(formData.get("password"));
+
+  const div = document.querySelector("#info");
+
+  if (checkPassword()) {
+    div.innerText = "";
+    const res = await fetch("/signup", {
+      method: "post",
+      body: formData,
+    });
+    const data = await res;
+    if(res==="200") div.innerText="회원가입이 성공했습니다.";
+  } 
+  else {
+    div.innerText = "비밀번호가 같지 않습니다.";
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
--- CarrotMarketCloneCoding/main.py
+++ CarrotMarketCloneCoding/main.py
@@ -3,23 +3,30 @@ from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
 
-con = sqlite3.connect("db.db", check_same_thread=False)
-cur = con.cursor()
+#con = sqlite3.connect("db.db", check_same_thread=False)
+#cur = con.cursor()
 
 app = FastAPI()
 
-@app.post('/items')
-async def create_item(image:UploadFile,
-                title:Annotated[str,Form()],
-                price:Annotated[int,Form()],
-                description:Annotated[str,Form()],
-                place:Annotated[str,Form()]):
-    image_bytes = await image.read() #image 읽는 시간 필요
-    cur.execute(f"""
-                INSERT INTO items(title, image, price, description, place) 
-                VALUES ('{title}','{image_bytes.hex()}', {price}, '{description}', '{place}') 
-                """) #'{문자열}' {숫자}, hex()->16진수로 변환(데이터 길이 줄이기)
-    con.commit()
+###
+#@app.post('/items')
+#async def create_item(image:UploadFile,
+#                title:Annotated[str,Form()],
+#                price:Annotated[int,Form()],
+#                description:Annotated[str,Form()],
+#                place:Annotated[str,Form()]):
+#    image_bytes = await image.read() #image 읽는 시간 필요
+#    cur.execute(f"""
+#                INSERT INTO items(title, image, price, description, place) 
+#                VALUES ('{title}','{image_bytes.hex()}', {price}, '{description}', '{place}') 
+#                """) #'{문자열}' {숫자}, hex()->16진수로 변환(데이터 길이 줄이기)
+#    con.commit()
+#    return '200'
+###
+
+@app.post("/signup")
+def signup(id:Annotated[str,Form()], password:Annotated[str, Form()]):
+    print(id, password)
     return '200'
 
 app.mount("/", StaticFiles(directory="SuperCoding/CarrotMarketCloneCoding/frontend", html=True), name="frontend")
